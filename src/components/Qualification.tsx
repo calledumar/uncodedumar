5065d3eb1dd9a7853e6f71e551dab60e052fe531
Muhammad Umar Riaz <chmuhammadumar7@gmail.com>
2025-08-09 01:26:35 +0500

    Update Qualification.tsx

--- src/components/Qualification.tsx
+++ src/components/Qualification.tsx
@@ -30,6 +30,33 @@ export default function Qualification() {
           </div>
         </div>
       </div>
+ {/* Small Image */}
+          <div className="flex-shrink-0 transition-transform duration-500 group-hover:scale-110 group-hover:rotate-3">
+            <img
+              src="https://share.google/RqmFY1lQHq32VZ9cO"
+              alt="Content Image"
+              className="w-30 h-30 md:w-40 md:h-40 lg:w-40 lg:h-40 object-cover rounded-lg shadow-lg transition-all duration-500 group-hover:shadow-blue-500/30 group-hover:shadow-xl"
+            />
+          </div>
+
+          {/* Text Content */}
+          <div className="flex-1 text-center md:text-left transition-all duration-500 group-hover:translate-x-2">
+            <h3 className="text-1xl md:text-1xl lg:text-2xl font-bold text-white leading-tight mb-4 transition-all duration-300 group-hover:text-blue-400 group-hover:scale-105">
+              Forward Fellowship Program | 2025
+            </h3>
+
+            <h3 className="text-1xl md:text-1xl lg:text-1xl font-bold text-gray-100 leading-tight mb-6 transition-all duration-300 group-hover:text-gray-50">
+              McKinsey & Co. (Academy), NY 
+            </h3>
+
+            <p className="text-base text-gray-300 leading-relaxed max-w-3xl transition-all duration-300 group-hover:text-gray-200 group-hover:leading-loose">
+              I successfully completed the McKinsey Forward Program, a globally recognized remote-learning initiative focused on developing future-ready skills. Through this journey, I deepened my understanding of structured problem-solving, corporate communication, decision-making, emotional intelligence, and the fundamentals of AI in a business context—all through hands-on, reflective learning. View my verified digital badge on <a href="https://www.credly.com/badges/01740a80-6794-4553-b627-81d9a074ea3a"> Creadly</a>
+
+
+            </p>
+          </div>
+        </div>
+      </div>
 
       <div className="max-w-7xl m-6 mx-auto">
         <div className="flex flex-col md:flex-row items-center md:items-start gap-8 md:gap-12 lg:gap-16 group cursor-pointer transition-all duration-500 hover:bg-gray-900/30 rounded-xl p-6 hover:shadow-2xl hover:shadow-red-500/10">
